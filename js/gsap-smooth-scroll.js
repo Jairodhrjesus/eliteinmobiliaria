c6989b762c6bb836531c45e2a182101398a48478
Jairo Hurtado <Jairodehurtado@gmail.com>
2025-06-03 16:03:39 -0500

    Estetia y funcionalidad con gsap

---
+++ js/gsap-smooth-scroll.js
@@ -0,0 +1,45 @@
+// Smooth scroll con GSAP
+// Requiere GSAP y ScrollToPlugin
+
+// Scroll suave global con GSAP ScrollSmoother
+(function() {
+    function loadGSAPandSmoother(callback) {
+        function loadScript(src, onload) {
+            var s = document.createElement('script');
+            s.src = src;
+            s.onload = onload;
+            document.head.appendChild(s);
+        }
+        if (typeof gsap === 'undefined') {
+            loadScript('https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js', function() {
+                if (typeof ScrollSmoother === 'undefined') {
+                    loadScript('https://assets.codepen.io/16327/ScrollSmoother.min.js', callback);
+                } else {
+                    callback();
+                }
+            });
+        } else if (typeof ScrollSmoother === 'undefined') {
+            loadScript('https://assets.codepen.io/16327/ScrollSmoother.min.js', callback);
+        } else {
+            callback();
+        }
+    }
+
+    function initSmoother() {
+        if (typeof ScrollSmoother !== 'undefined') {
+            ScrollSmoother.create({
+                smooth: 1.2,
+                effects: true
+            });
+        }
+    }
+
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', function() {
+            loadGSAPandSmoother(initSmoother);
+        });
+    } else {
+        loadGSAPandSmoother(initSmoother);
+    }
+})();
+
